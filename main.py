37cd91e8d2acd2ca726a4e84e7be02637d5a17b1
karma007 <abhishekpandey.kr@gmail.com>
2022-01-16 19:15:34 +0530

    wip automation of internet complainer

--- main.py
+++ main.py
@@ -14,14 +14,15 @@ class InternetSpeedBot:
         self.driver.get("https://www.speedtest.net/")
         go_btn = self.driver.find_element_by_class_name("js-start-test")
         go_btn.click()
-        time.sleep(60)
+        time.sleep(20)
+        internet_provider = self.driver.find_element_by_class_name("result-label")
+        print(internet_provider.text)
+        time.sleep(40)
         download_speed = self.driver.find_element_by_class_name("download-speed").text
         upload_speed = self.driver.find_element_by_class_name("upload-speed").text
         print(download_speed, upload_speed)
         if download_speed and upload_speed:
             self.tweet_at_internet_provider(download_speed, upload_speed)
-
-        pass
 
     def tweet_at_internet_provider(self, down_speed=0, up_speed=0):
         self.driver.get("https://twitter.com/i/flow/login")
@@ -31,14 +32,20 @@ class InternetSpeedBot:
         input_field.send_keys(user_input)
         next_btn = self.driver.find_element_by_xpath('//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[6]')
         next_btn.click()
+        time.sleep(5)
         pwd_feild = self.driver.find_element_by_name("password")
         pwd = input("Please Enter Password:")
         pwd_feild.send_keys(pwd)
-
-
+        login_btn = self.driver.find_element_by_xpath('//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div')
+        login_btn.click()
+        tweet_editor = self.driver.find_element_by_class_name("DraftEditor-root")
+        tweet_editor.send_keys("Hi Internet Provider, Your Internet Speed is not what it was Promised.")
+        time.sleep(2)
+        tweet_btn = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[3]/div/div/div[2]')
+        tweet_btn.click()
 
 
 
 bot = InternetSpeedBot()
 time.sleep(2)
-bot.tweet_at_internet_provider()
+bot.get_internet_speed()
