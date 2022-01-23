4a9ba3783c31b2da57fcb4b22cf7cda5608695e6
karma007 <abhishekpandey.kr@gmail.com>
2022-01-23 11:17:00 +0530

    team logo scrapping

--- ipl-scrapping/main.py
+++ ipl-scrapping/main.py
@@ -15,6 +15,7 @@ for item in range(0, total_teams):
     team = {}
     score_container = driver.find_element_by_class_name(f"team{item}")
     name = score_container.find_element_by_css_selector(".team .ap-team-wrp a").text
+    logo = score_container.find_element_by_css_selector(".team .ap-team-ico img").get_attribute('src')
     played = score_container.find_element_by_css_selector(".pld .ap-team-name").text
     netrr = score_container.find_element_by_css_selector('.netrr .ap-team-name').text
     pts = score_container.find_element_by_css_selector('.pts .ap-team-name').text
@@ -22,6 +23,7 @@ for item in range(0, total_teams):
     team['played'] = played
     team['netrr'] = netrr
     team['pts'] = pts
+    team['logo'] = logo
     teams.append(team)
 
 # write this data to json file
